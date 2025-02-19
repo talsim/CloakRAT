721a87d40a862f6a1f9ed7e770af36e2797c1dab
talsim <talsimhayev@gmail.com>
2025-02-19 22:08:15 +0200

    Add modulo in the re-encryption loop because of cyclic XOR key and change the re-encryption function to get as a param the length of the string

--- Shared/string_encryption.cpp
+++ Shared/string_encryption.cpp
@@ -4,14 +4,14 @@
 // At runtime, we will introduce a dynamic key that will replace the compile-time encryption and be random - the effective XOR key.
 // Then we will re-encrypt the strings on program startup (maybe in the tls callback) using the effective key.
 
-std::array<uint8_t, RUNTIME_KEY_LENGTH> generate_runtime_key() // TODO: Might be better inlined for stealth cuz this is a super sensitive func
+std::array<uint8_t, DYNAMIC_KEY_LENGTH> generate_runtime_key() // TODO: Might be better inlined for stealth cuz this is a super sensitive func
 {
-	std::array<uint8_t, RUNTIME_KEY_LENGTH> runtime_key = {}; // dynamic key
+	std::array<uint8_t, DYNAMIC_KEY_LENGTH> runtime_key = {}; // dynamic key
 	std::random_device rd; // Used as the seed
 	std::mt19937 rng(rd()); // Our PRNG 
 	std::uniform_int_distribution<int> dist(0, 255); // Get random numbers between 0 to 255 (1 byte)
 
-	for (int i = 0; i < RUNTIME_KEY_LENGTH; i++)
+	for (int i = 0; i < DYNAMIC_KEY_LENGTH; i++)
 	{
 		small_junk();
 		runtime_key[i] = (uint8_t)(dist(rng));
@@ -26,23 +26,33 @@ std::array<uint8_t, RUNTIME_KEY_LENGTH> generate_runtime_key() // TODO: Might be
 	return runtime_key;
 }
 
-void runtime_reencryption(char *data)
+void runtime_reencryption(char* data, size_t data_length)
 {
 	/*
-	* Our compile-time encryption: E = plaintext XOR compile_time_key
-	* We want final data:          E' = plaintext XOR runtime_key
-	* Thus, the runtime re-encryption is as follows: data[i] ^ (compile_time_key[i] ^ runtime_key[i])
+	* Our compile-time encryption: E = string XOR compile_time_key
+	* We want final data:          E' = string XOR dynamic_key
+	* Thus, the runtime re-encryption is as follows: data[i] ^ (compile_time_key[i] ^ dynamic_key[i])
 	* where data = E
 	*/
 	
 	junk();
-	std::array<uint8_t, RUNTIME_KEY_LENGTH> runtime_key = generate_runtime_key();
-	for (int i = 0; i < strlen(data); i++)
+	std::array<uint8_t, DYNAMIC_KEY_LENGTH> dynamic_key = generate_runtime_key();
+	for (int i = 0; i < data_length; i++)
 	{
 		suspicious_junk_1();
-		data[i] = data[i] ^ (char)(COMPILE_TIME_KEY[i] ^ runtime_key[i]);
+		
+		data[i] = data[i] ^ (char)(COMPILE_TIME_CIPHER ^ dynamic_key[i % DYNAMIC_KEY_LENGTH]/* TODO: define a macro like RUNTIME_CIPHER*/);
 	}
 }
 
+
+std::string decrypt(char* data, size_t data_length)
+{
+	std::string result = "";
+
+	// TODO: Implement.
+
+	return result;
+}
 
 
--- Shared/string_encryption.h
+++ Shared/string_encryption.h
@@ -5,9 +5,10 @@
 #include <random>
 #include "junk_codes.h"
 
-#define RUNTIME_KEY_LENGTH 16
+#define DYNAMIC_KEY_LENGTH 16
+#define COMPILE_TIME_CIPHER (i % 4 | ((i * 9) / 2 + COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()] & i * i - COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()]) << i) // Add random ops to make the XOR obfuscation more unique
 
-std::array<uint8_t, RUNTIME_KEY_LENGTH> generate_runtime_key();
+std::array<uint8_t, DYNAMIC_KEY_LENGTH> generate_runtime_key();
 
 // 16 byte compile-time XOR key
 constexpr std::array<uint8_t, 16> COMPILE_TIME_KEY = { 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A' };
@@ -17,6 +18,6 @@ constexpr std::array<char, N> compile_time_encrypt(const char (&str)[N] /* Impor
 {
 	std::array<char, N> encryptedArray = {}; 
 	for (int i = 0; i < N; i++)
-		encryptedArray[i] = str[i] ^ (char)(i % 4 | ((i * 9) / 2 + COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()] & i * i - COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()]) << i); // the XOR cipher
+		encryptedArray[i] = str[i] ^ (char)COMPILE_TIME_CIPHER; // Avoid typical XOR obfuscation, to not get detected easily
 	return encryptedArray;
 }
