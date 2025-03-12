6f4003eb7783e68664fbfa4280ff09409a31f017
talsim <talsimhayev@gmail.com>
2025-03-12 18:06:24 +0200

    Add main() implementation in the encrypt_strings.py script

--- encrypt_strings.py
+++ encrypt_strings.py
@@ -1,9 +1,8 @@
-import os
 import secrets
 
 HEADER_NAME = 'encrypted_strings_autogen.h'
 SOURCE_NAME = 'encrypted_strings_autogen.cpp'
-DIR = 'Shared/'
+DIR = 'Shared'
 KEY_ENTROPY = 16 # bytes
 # COMPILE_TIME_CIPHER_BYTE = ((i % 4 | ((i * 9) / 2 + COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()] & ((i/2)>>3) * i - COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()]) << i) ^ 0x9F) // Add random ops to make the XOR obfuscation more unique
 
@@ -13,6 +12,7 @@ KEY_ENTROPY = 16 # bytes
 strings_to_encrypt = {
     'str_ip': '127.0.0.1',
     'str_cmd': 'cmd.exe /C',
+    'str_dllPath': '.....................',
     'str_kernel32': 'kernel32.dll',
 }
 
@@ -28,17 +28,25 @@ def xor_encrypt(string_plaintext: str) -> list[int]:
     pass
 
 def to_c_array(variable_name: str, buffer: list[int]) -> str:
+    # return in the format: "unsigned char[] var1 = { ... };"
     pass
 
 def to_extern_decl(variable_name: str) -> str:
+    # return in the format: "extern unsigned char var1[];"
     pass
     
 
 def main():
     key = keygen()
-    # create the files.
-    # loop through the strings_to_encrypt dict
-    
+    with open(f'{DIR}/{SOURCE_NAME}', 'w') as source_file:
+        for var_name, string in strings_to_encrypt.items():
+            encrypted_string_lst = xor_encrypt(string)
+            encrypted_c_arr = to_c_array(var_name, encrypted_string_lst)
+            source_file.write(encrypted_c_arr + '\n')
+    with open(HEADER_NAME, 'w') as header_file:
+        for var_name in strings_to_encrypt.keys():
+            extern_decl = to_extern_decl(var_name)
+            header_file.write(extern_decl + '\n')
 
 if __name__ == '__main__':
         main()
