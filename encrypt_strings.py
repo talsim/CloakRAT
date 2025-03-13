20ee7fe517f0589acc08ad8c39192dbd9cd05338
talsim <talsimhayev@gmail.com>
2025-03-13 21:17:29 +0200

    Add minor changes

--- encrypt_strings.py
+++ encrypt_strings.py
@@ -5,9 +5,6 @@ SOURCE_NAME = 'encrypted_strings_autogen.cpp'
 DIR = 'Shared'
 KEY_ENTROPY = 16 # bytes
 
-# COMPILE_TIME_CIPHER_BYTE = ((i % 4 | ((i * 9) / 2 + COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()] & ((i/2)>>3) * i - COMPILE_TIME_KEY[i % COMPILE_TIME_KEY.size()]) << i) ^ 0x9F) // Add random ops to make the XOR obfuscation more unique
-
-
 
 # Add or modify strings here.
 strings_to_encrypt = {
@@ -24,22 +21,24 @@ def gen_key() -> list[int]:
     return key
 
 def get_random_op() -> str:
-    return secrets.choice(['+', '-', '*', '%'])
+    return secrets.choice(['+', '-', '*'])
 
-def xor_encrypt(string_plaintext: str, key: list[int], cipher: str) -> list[int]:
+def xor_encrypt(string_plaintext: str, key: list[int]) -> list[int]: # TODO: cast each encrypted byte to really a size of a byte.
     # loop through the plaintext and just xor it with the cipher
     encrypted_bytes = []
     data = string_plaintext.encode('utf-8') + b'\x00' # add the null terminator so it will be encrypted too
-    for i, byte in enumerate(data):
-        encrypted_bytes.append(byte ^ eval(cipher))
+    byte_cipher = f"((i % 4 | ((i {get_random_op()} 9) {get_random_op()} 2 + key[i % len(key)] & ((i//2)>>3) * i {get_random_op()} key[i % len(key)]) << i) ^ {secrets.randbelow(256)})" # randomize the ops - BE CAREFUL TO EDIT VARIABLE NAMES, THIS IS GOING TO BE PROCESSED BY eval()
+
+    for i, byte in enumerate(data):        
+        encrypted_bytes.append(byte ^ eval(byte_cipher))
     
     return encrypted_bytes
     
 
 def to_c_array(variable_name: str, buffer: list[int]) -> str:
     # return in the format: "unsigned char[] variable_name = { ...buffer };"
-    #elems = ', '.join(str(b) for b in buffer)
-    return f"unsigned char[] {variable_name} = {{ {buffer} }};" 
+    elems = ', '.join(str(b) for b in buffer)
+    return f"unsigned char {variable_name}[] = {{ {elems} }};" 
     
 
 def to_extern_decl(variable_name: str) -> str:
@@ -48,16 +47,15 @@ def to_extern_decl(variable_name: str) -> str:
     
 
 def main():
-    key = gen_key()
-    byte_cipher = f"((i % 4 | ((i {get_random_op()} 9) / 2 + key[i % len(key)] & ((i/2)>>3) * i {get_random_op()} key[i % len(key)]) << i) ^ {secrets.randbelow(256)})" # randomize the ops - BE CAREFUL TO EDIT VARIABLE NAMES, THIS IS GOING TO BE PROCESSED BY eval()
+    key = gen_key()    
     
     with open(f'{DIR}/{SOURCE_NAME}', 'w') as source_file:
         for var_name, string in strings_to_encrypt.items():
-            encrypted_string_lst = xor_encrypt(string, key, byte_cipher)
+            encrypted_string_lst = xor_encrypt(string, key)
             encrypted_c_arr = to_c_array(var_name, encrypted_string_lst)
             source_file.write(encrypted_c_arr + '\n')
             
-    with open(HEADER_NAME, 'w') as header_file:
+    with open(f'{DIR}/{HEADER_NAME}', 'w') as header_file:
         for var_name in strings_to_encrypt.keys():
             extern_decl = to_extern_decl(var_name)
             header_file.write(extern_decl + '\n')
