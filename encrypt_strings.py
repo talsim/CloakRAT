4fa126745b44ad2756b0577184c21da5ddbfe828
talsim <talsimhayev@gmail.com>
2025-03-20 19:58:28 +0200

    Add to_c_struct()

--- encrypt_strings.py
+++ encrypt_strings.py
@@ -71,16 +71,18 @@ def xor_encrypt(string_plaintext: str, key: list[int], cipher: str) -> list[int]
     
     return encrypted_bytes
     
+def to_c_struct(variable_name: str):
+    #typedef struct EncryptedString {{\n    unsigned char* data;\n    size_t length;\n}} EncryptedString;\n
+    return f'EncryptedString {variable_name} = {{\n    {variable_name}_data,\n    sizeof({variable_name}_data)\n}};'
 
 def to_c_array(variable_name: str, buffer: list[int]) -> str:
     # return in the format: "unsigned char variable_name[] = { ...buffer };"
     elems = ', '.join(str(b) for b in buffer)
-    return f"unsigned char {variable_name}[] = {{ {elems} }};" 
-    
+    return f"unsigned char {variable_name}[] = {{ {elems} }};"
 
 def to_extern_decl(variable_name: str) -> str:
-    # return in the format: "extern unsigned char variable_name[];"
-    return f"extern unsigned char {variable_name}[];"
+    # return in the format: "extern EncryptedString variable_name;"
+    return f"extern EncryptedString {variable_name};"
     
 
 def main():    
@@ -91,7 +93,7 @@ def main():
     rand_op3 = get_random_op()
     rand_xor_value = secrets.randbelow(256)
     
-    # BE CAREFUL TO EDIT VARIABLE NAMES HERE, BECAUSE THIS IS GOING TO BE PROCESSED BY eval()
+    # BE CAREFUL TO EDIT VARIABLE NAMES HERE, THIS IS GOING TO BE PROCESSED BY eval()
     byte_chiper = f"((i % 4 | ((i {rand_op1} 9) {rand_op2} 2 + key[i % len(key)] & ((i//2)>>3) * i {rand_op3} key[i % len(key)]) << (i % 5)) & 0x7F ^ {rand_xor_value})" # randomize the cipher each run
     
     # Generate the Source file 
@@ -99,10 +101,12 @@ def main():
         source_file.write(f'#include \"{HEADER_NAME}\"\n\n')
         
         for var_name, string in strings_to_encrypt.items():
+            var_name = f'{var_name}'
             encrypted_string_lst = xor_encrypt(string, key, byte_chiper)
-            encrypted_c_arr = to_c_array(var_name, encrypted_string_lst)
+            encrypted_c_arr = to_c_array(f'{var_name}_data', encrypted_string_lst)
+            c_struct = to_c_struct(var_name)
             source_file.write(encrypted_c_arr + '\n')  # Write the encrypted string array
-            source_file.write(f'size_t {var_name}_len = sizeof({var_name});\n\n')  # Write the length of the array
+            source_file.write(c_struct + '\n\n')  # Write the encrypted string struct
     
     # Generate the Header file
     with open(f'{DIR}/{HEADER_NAME}', 'w') as header_file:
@@ -112,11 +116,11 @@ def main():
         header_file.write('#include <array>\n\n')
         header_file.write(f'#define {HEADER_XOR_CIPHER_VARIABLE_NAME} {c_style_byte_cipher}\n')
         header_file.write(f'static std::array<uint8_t, {KEY_ENTROPY}> {HEADER_XOR_KEY_VARIABLE_NAME} = {{ {', '.join(str(b) for b in key)} }};\n\n')
+        header_file.write(f'typedef struct EncryptedString {{\n    unsigned char* data;\n    size_t length;\n}} EncryptedString;\n\n')
         
         for var_name in strings_to_encrypt.keys():
             extern_decl = to_extern_decl(var_name)
             header_file.write(extern_decl + '\n')
-            header_file.write(f'extern size_t {var_name}_len;\n\n')
 
 if __name__ == '__main__':
         main()
